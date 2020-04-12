0c742423fc12018be7ea2f1a7c884109308d93ca
Mateus Ferreira Silva <mtsferreirasilva@gmail.com>
2020-04-11 23:35:36 -0400

    Coxinha scores

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -1,14 +1,29 @@
 import Phaser from 'phaser';
 import { CST } from '../CST';
-// import { fontStyles } from '../config';
+import { fontStyles } from '../config';
 
 export default class GameScene extends Phaser.Scene {
   constructor() {
     super({ key: CST.SCENES.GAME });
+    this.score = 0;
   }
 
   create() {
     this.renderBackgroundImage();
+    this.renderScore();
+  }
+
+  renderScore() {
+    const { renderer } = this.game;
+
+    this.add
+      .text(
+        20,
+        renderer.height - 50,
+        `Coxinhas: ${this.score}`,
+        { ...fontStyles, font: "30px Helvetica" },
+      )
+      .setOrigin(0);
   }
 
   renderBackgroundImage() {
